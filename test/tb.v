e33a80fc77176590b97386f6cd642cecab62019e
Hugh Ding <hzfmd2006@gmail.com>
2026-09-21 22:10:21 -0400

    edit tb.v

--- test/tb.v
+++ test/tb.v
@@ -27,8 +27,8 @@ module tb ();
   wire VGND = 1'b0;
 `endif
 
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  // Replaced tt_um_example with tt_um_8_bit_counter:
+  tt_um_8_bit_counter user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
